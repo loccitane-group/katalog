dac54aff600b86c3c589fdeb13c3ef8284c91d0f
Anthony Joyeux <anthony.joyeux@loccitane.com>
2022-08-25 13:48:42 +0200

    feature : multiple of 3 return Fizz

--- FizzBuzz/Kotlin/src/main/kotlin/com/loccitane/kata/FizzBuzz.kt
+++ FizzBuzz/Kotlin/src/main/kotlin/com/loccitane/kata/FizzBuzz.kt
@@ -2,6 +2,8 @@ package com.loccitane.kata
 
 class FizzBuzz {
     fun run(number: Int): String {
+        if (number % 3 == 0)
+            return "Fizz"
         return number.toString()
     }
 }
--- FizzBuzz/Kotlin/src/test/kotlin/com/loccitane/kata/FizzBuzzTest.kt
+++ FizzBuzz/Kotlin/src/test/kotlin/com/loccitane/kata/FizzBuzzTest.kt
@@ -8,4 +8,9 @@ class FizzBuzzTest {
     fun `Should return 1 given 1 in param`() {
         assertThat(FizzBuzz().run(1)).isEqualTo("1")
     }
+    @Test
+    fun `Should return fizz if multiple of 3`() {
+        assertThat(FizzBuzz().run(6)).isEqualTo("Fizz")
+    }
+
 }
