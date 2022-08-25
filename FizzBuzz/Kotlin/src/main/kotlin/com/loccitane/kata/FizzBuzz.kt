b4f8604f6a3ca161884ac53f95e1aa244f126a48
Anthony Joyeux <anthony.joyeux@loccitane.com>
2022-08-25 13:50:58 +0200

    feature : multiple of 5 return Fizz

--- FizzBuzz/Kotlin/src/main/kotlin/com/loccitane/kata/FizzBuzz.kt
+++ FizzBuzz/Kotlin/src/main/kotlin/com/loccitane/kata/FizzBuzz.kt
@@ -4,6 +4,8 @@ class FizzBuzz {
     fun run(number: Int): String {
         if (number % 3 == 0)
             return "Fizz"
+        if (number % 5 == 0)
+            return "Buzz"
         return number.toString()
     }
 }
--- FizzBuzz/Kotlin/src/test/kotlin/com/loccitane/kata/FizzBuzzTest.kt
+++ FizzBuzz/Kotlin/src/test/kotlin/com/loccitane/kata/FizzBuzzTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class FizzBuzzTest {
+
     @Test
     fun `Should return 1 given 1 in param`() {
         assertThat(FizzBuzz().run(1)).isEqualTo("1")
@@ -12,5 +13,8 @@ class FizzBuzzTest {
     fun `Should return fizz if multiple of 3`() {
         assertThat(FizzBuzz().run(6)).isEqualTo("Fizz")
     }
-
+    @Test
+    fun `Should return fizz if multiple of 5`() {
+        assertThat(FizzBuzz().run(10)).isEqualTo("Buzz")
+    }
 }
