38fe10a6380e84b47dad08bfd1eee8062e385c98
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-09-13 20:13:12 +0200

    Alex solution - no tail rec

--- Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
+++ Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
@@ -1,9 +1,55 @@
 package com.katas.romanNumerals
 
 class ArabicNumerals {
-    def romanDictionnary = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
-   def getRoman (def myNumber){
-       romanDictionnary[myNumber]
+    def romanBits = [
+            ["I", "V"], // < 9, we need "I" and "V"
+            ["X", "L"],
+            ["C", "D"],
+            ["M", "?"],
+            ["!", "§"]
+    ]
+
+    /**
+     * Just take care of one arabic digit
+     * @param n in [0, 10[
+     * @param log10 >=0
+     * @return
+     */
+    String getRomanChunk(Integer n, Integer log10){
+        def bit1 = romanBits[log10][0]
+        def bit5 = romanBits[log10][1]
+        def bit10 = romanBits[log10+1][0]
+        switch(n) {
+            case 0..3:
+                bit1 * n
+                break
+            case 4:
+                "${bit1}${bit5}"
+                break
+            case 5..8:
+                "${bit5}${bit1*(n-5)}"
+                break
+            case 9:
+                "${bit1}${bit10}"
+        }
+    }
+    /**
+     * the recursive handler for getRoman
+     * prepends getRomanHandler with remain/10 and log10+1 to getRomanChunk(remain%10, log10)
+     * @param remain the number to convert
+     * @param log10 starts with 0, and increment each time we go to the next power of 10
+     * @return
+     */
+    private String getRomanHandler(Integer remain, Integer log10){
+        if (remain == 0){
+            return ''
+        }
+        def digit = remain % 10
+        Integer next = remain /10
+        return getRomanHandler(next, log10+1)+getRomanChunk(digit, log10)
+    }
+    def getRoman(def myNumber) {
+        getRomanHandler(myNumber, 0)
     }
 }
--- Groovy/src/test/groovy/com/katas/romanNumerals/RomanNumeralSpec.groovy
+++ Groovy/src/test/groovy/com/katas/romanNumerals/RomanNumeralSpec.groovy
@@ -6,6 +6,30 @@ import spock.lang.Unroll
 class RomanNumeralSpec  extends Specification{
 
     @Unroll
+    def "getRomanChunk convert one digit, given the log10"() {
+        given:
+         def arabicNumerals = new ArabicNumerals()
+
+        expect:
+        arabicNumerals.getRomanChunk(arabic, log10) == expected
+
+        where:
+        arabic || log10 || expected
+        1      || 0     || 'I'
+        2      || 0     || 'II'
+        3      || 0     || 'III'
+        4      || 0     || 'IV'
+        5      || 0     || 'V'
+        6      || 0     || 'VI'
+        7      || 0     || 'VII'
+        8      || 0     || 'VIII'
+        9      || 0     || 'IX'
+        0      || 1     || ''
+        1      || 1     || 'X'
+        2      || 1     || 'XX'
+        6      || 1     || 'LX'
+    }
+  @Unroll
     def "Can convert arabic #arabic to roman #expected"() {
         given:
          def arabicNumerals = new ArabicNumerals()
