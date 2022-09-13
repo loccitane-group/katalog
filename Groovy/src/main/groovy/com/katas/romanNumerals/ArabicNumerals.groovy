f589453bfa70170b8c90614661a1f0cd48a28890
Fabien Giannesini <fabien.giannesini.ext@loccitane.com>
2022-09-13 14:19:42 +0200

    Implements 27

--- Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
+++ Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
@@ -6,6 +6,7 @@ class ArabicNumerals {
         (1..myNumber)
                 .collect { "I" }
                 .join()
+                .replaceAll("I{10}", "X")
                 .replaceAll("I{9}", "IX")
                 .replaceAll("I{5}", "V")
                 .replaceAll("I{4}", "IV")
