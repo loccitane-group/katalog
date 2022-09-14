5b543057f531aa5b269c1ed6caee422b5c72d84a
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-09-14 08:16:09 +0200

    Fabien idea, no regexp :(

--- Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
+++ Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
@@ -1,9 +1,31 @@
 package com.katas.romanNumerals
 
 class ArabicNumerals {
-    def romanDictionnary = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    def n2romanChunk = [
+            [1000, "M"],
+            [900, "CM"],
+            [500, "D"],
+            [400, "CD"],
+            [100, "C"],
+            [90, "XC"],
+            [50, "L"],
+            [40, "XL"],
+            [10, "X"],
+            [9, "IX"],
+            [5, "V"],
+            [4, "IV"],
+            [1, "I"]
+    ]
 
-   def getRoman (def myNumber){
-       romanDictionnary[myNumber]
+
+    def getRoman(def myNumber) {
+        String acc = ""
+        Integer remain = myNumber
+        while (remain > 0) {
+            def rep = n2romanChunk.find({ it[0] <= remain })
+            acc += rep[1]
+            remain -= rep[0]
+        }
+        return acc
     }
 }
