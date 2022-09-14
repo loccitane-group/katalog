ab3f3144c24c50c6efe61e36bfe7f31a97199527
vincent_plourde <vincent.plourde@loccitane.com>
2022-09-14 09:26:17 +0200

    Adding code for managing case from 10 to 4999

--- Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
+++ Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
@@ -1,9 +1,26 @@
 package com.katas.romanNumerals
 
 class ArabicNumerals {
-    def romanDictionnary = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    def romanDictionnaryNumber = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    def romanDictionnaryTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    def romanDictionnaryHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    def romanDictionnaryThousands = ["", "M", "MM", "MMM", "MMMM"]
 
    def getRoman (def myNumber){
-       romanDictionnary[myNumber]
+       if (myNumber>=5000){
+           "Not handled"
+       }
+       else if (myNumber>=1000){
+           romanDictionnaryThousands[myNumber/1000]+romanDictionnaryHundreds[myNumber%1000/100]+romanDictionnaryTens[(myNumber%100)/10]+romanDictionnaryNumber[myNumber%10]
+       }
+       else if (myNumber>=100){
+           romanDictionnaryHundreds[myNumber/100]+romanDictionnaryTens[(myNumber%100)/10]+romanDictionnaryNumber[myNumber%10]
+       }
+       else if(myNumber>=10){
+           romanDictionnaryTens[myNumber/10]+romanDictionnaryNumber[myNumber%10]
+       }
+       else {
+           romanDictionnaryNumber[myNumber]
+       }
     }
 }
