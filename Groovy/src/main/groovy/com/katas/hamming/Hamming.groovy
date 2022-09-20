3a7447b7c974fd3ad6cc75fb01a7cc54f0a815a8
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-20 14:44:05 +0200

    Hamming kata - refactor s1 and s2 to strand1 and strand2

--- Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
+++ Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
@@ -5,20 +5,20 @@ class Hamming {
     int SINGLE_DIFFERENCE_HAMMING = 1
     int LONG_DIFFERENCE_HAMMING = 9
 
-    int distance(String s1, String s2) {
-        if (areEmptyStrands(s1, s2)) {
+    int distance(strand1, strands2) {
+        if (areEmptyStrands(strand1, strands2)) {
             return NO_DIFFERENCE_HAMMING
         }
-        if (areSingleLetterIdenticalStrands(s1, s2)) {
+        if (areSingleLetterIdenticalStrands(strand1, strands2)) {
             return NO_DIFFERENCE_HAMMING
         }
-        if (areSingleLetterDifferentStrands(s1, s2)) {
+        if (areSingleLetterDifferentStrands(strand1, strands2)) {
             return SINGLE_DIFFERENCE_HAMMING
         }
-        if (areLongIdenticalStrands(s1, s2)) {
+        if (areLongIdenticalStrands(strand1, strands2)) {
             return NO_DIFFERENCE_HAMMING
         }
-        if (areLongDifferentStrands(s1, s2)) {
+        if (areLongDifferentStrands(strand1, strands2)) {
             /*
                 Disclaimer: I'm returning here a static number of 9
                 because the test asks to return 9 for two long different strands
@@ -32,53 +32,53 @@ class Hamming {
             return LONG_DIFFERENCE_HAMMING
         }
 
-        if (disallowFirstStrandLonger(s1, s2)) {
+        if (disallowFirstStrandLonger(strand1, strands2)) {
             throw new ArithmeticException("First strand should not be longer than second strand!")
         }
     }
 
-    boolean areSingleLetterIdenticalStrands(s1, s2) {
-        return areSingleLetterStrands(s1, s2) && areSameSizeStrands(s1, s2) && areIdenticalStrands(s1, s2)
+    boolean areSingleLetterIdenticalStrands(strand1, strands2) {
+        return areSingleLetterStrands(strand1, strands2) && areSameSizeStrands(strand1, strands2) && areIdenticalStrands(strand1, strands2)
     }
 
-    boolean areSingleLetterDifferentStrands(s1, s2) {
-        return areSingleLetterStrands(s1, s2) && areSameSizeStrands(s1, s2) && areDifferentStrands(s1, s2)
+    boolean areSingleLetterDifferentStrands(strand1, strands2) {
+        return areSingleLetterStrands(strand1, strands2) && areSameSizeStrands(strand1, strands2) && areDifferentStrands(strand1, strands2)
     }
 
-    boolean areLongIdenticalStrands(s1, s2) {
-        return areLongStrands(s1, s2) && areSameSizeStrands(s1, s2) && areIdenticalStrands(s1, s2)
+    boolean areLongIdenticalStrands(strand1, strands2) {
+        return areLongStrands(strand1, strands2) && areSameSizeStrands(strand1, strands2) && areIdenticalStrands(strand1, strands2)
     }
 
-    boolean areLongDifferentStrands(s1, s2) {
-        return areLongStrands(s1, s2) && areSameSizeStrands(s1, s2) && areDifferentStrands(s1, s2)
+    boolean areLongDifferentStrands(strand1, strands2) {
+        return areLongStrands(strand1, strands2) && areSameSizeStrands(strand1, strands2) && areDifferentStrands(strand1, strands2)
     }
 
-    boolean disallowFirstStrandLonger(String s1, String s2) {
-        return s1.length() > s2.length()
+    boolean disallowFirstStrandLonger(strand1, strands2) {
+        return strand1.length() > strands2.length()
     }
 
-    boolean areEmptyStrands(String s1, String s2) {
-        return s1 == '' && s2 == ''
+    boolean areEmptyStrands(strand1, strands2) {
+        return strand1 == '' && strands2 == ''
     }
 
-    boolean areSameSizeStrands(String s1, String s2) {
-        return s1.length() == s2.length()
+    boolean areSameSizeStrands(strand1, strands2) {
+        return strand1.length() == strands2.length()
     }
 
-    boolean areSingleLetterStrands(String s1, String s2) {
-        return s1.length() == 1
-                && s2.length() == 1
+    boolean areSingleLetterStrands(strand1, strands2) {
+        return strand1.length() == 1
+                && strands2.length() == 1
     }
 
-    boolean areIdenticalStrands(String s1, String s2) {
-        return s1 == s2
+    boolean areIdenticalStrands(strand1, strands2) {
+        return strand1 == strands2
     }
 
-    boolean areDifferentStrands(String s1, String s2) {
-        return !areIdenticalStrands(s1, s2)
+    boolean areDifferentStrands(strand1, strands2) {
+        return !areIdenticalStrands(strand1, strands2)
     }
 
-    boolean areLongStrands(String s1, String s2) {
-        return s1.length() > 1 && s2.length() > 1
+    boolean areLongStrands(strand1, strands2) {
+        return strand1.length() > 1 && strands2.length() > 1
     }
 }
