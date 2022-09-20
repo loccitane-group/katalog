57aa52cde867bd6b47ac140f0221f933b815d626
Jordan-Afonso-OCC <jordan.afonso.ext@loccitane.com>
2022-09-20 10:20:21 +0200

    implement arabic to roman trad

--- Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
+++ Groovy/src/main/groovy/com/katas/romanNumerals/ArabicNumerals.groovy
@@ -1,9 +1,31 @@
 package com.katas.romanNumerals
 
 class ArabicNumerals {
-    def romanDictionnary = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    final ROMAN_DICT = [
+            "M" : 1000,
+            "CM": 900,
+            "D" : 500,
+            "CD": 400,
+            "C" : 100,
+            "XC": 90,
+            "L" : 50,
+            "XL": 40,
+            "X" : 10,
+            "IX": 9,
+            "V" : 5,
+            "IV": 4,
+            "I" : 1
+    ]
 
-   def getRoman (def myNumber){
-       romanDictionnary[myNumber]
+    def getRoman(def myNumber) {
+        String romanTrad = ""
+        def remain = myNumber
+        ROMAN_DICT.each { romanDictEntry ->
+            while (remain >= romanDictEntry.value) {
+                romanTrad += romanDictEntry.key;
+                remain -= romanDictEntry.value;
+            }
+        }
+        return romanTrad
     }
 }
