b26acde268c77a32465040691b358b4caecbff11
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-20 14:21:38 +0200

    Hamming kata - refactor single letter equality to extract single letter logic and equality check logic in two separate functions

--- Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
+++ Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
@@ -8,10 +8,12 @@ class Hamming {
         if (areEmptyStrands(s1, s2)) {
             return NO_DIFFERENCE_HAMMING
         }
-        if (areIdenticalSingleLetterStrands(s1, s2)) {
+        if (areSingleLetterStrands(s1, s2)
+                && areIdenticalStrands(s1, s2)) {
             return NO_DIFFERENCE_HAMMING
         }
-        if (areDifferentSingleLetterStrands(s1, s2)) {
+        if (areSingleLetterStrands(s1, s2)
+                && areDifferentStrands(s1, s2)) {
             return SINGLE_DIFFERENCE_HAMMING
         }
         if (s1.length() > 1 && s2.length() > 1 && s1 == s2) {
@@ -23,13 +25,20 @@ class Hamming {
         return s1 == '' && s2 == ''
     }
 
-    boolean areIdenticalSingleLetterStrands(String s1, String s2) {
+    boolean areSingleLetterStrands(String s1, String s2) {
         return s1.length() == 1
                 && s2.length() == 1
-                && s1 == s2
     }
 
-    boolean areDifferentSingleLetterStrands(String s1, String s2) {
-        return !areIdenticalSingleLetterStrands(s1, s2)
+    boolean areIdenticalStrands(String s1, String s2) {
+        return s1 == s2
+    }
+
+    boolean areDifferentStrands(String s1, String s2) {
+        return !areIdenticalStrands(s1, s2)
+    }
+
+    boolean areLongIdenticalStrands(String s1, String s2) {
+        return s1.length() > 1 && s2.length() > 1 && s1 == s2
     }
 }
