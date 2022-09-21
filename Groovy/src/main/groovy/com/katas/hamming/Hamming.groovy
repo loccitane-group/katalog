1b4de49d56bb87e6d05bff6d9919a6aacd2ccb01
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-09-21 18:23:38 +0200

    not very original

--- Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
+++ Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
@@ -2,9 +2,9 @@ package com.katas.hamming
 
 class Hamming {
     int distance(String s1, String s2) {
-
-        if (s1.isEmpty() && s2.isEmpty()) 0
-        else if (s1 != s2) [s1.toCharArray(), s2.toCharArray()].transpose().findAll {n -> n[0] != n[1] }.size()
-         else 0
+        if (s1.length() != s2.length()) {
+            throw new ArithmeticException()
+        }
+        [s1.toCharArray(), s2.toCharArray()].transpose().findAll { n -> n[0] != n[1] }.size()
     }
 }
