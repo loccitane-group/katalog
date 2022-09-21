d995a4d70bcf856fab3f008bca4c224b08cbfc79
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-09-21 18:49:29 +0200

    un peu plus crypto rigolo

--- Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
+++ Groovy/src/main/groovy/com/katas/hamming/Hamming.groovy
@@ -2,9 +2,13 @@ package com.katas.hamming
 
 class Hamming {
     int distance(String s1, String s2) {
+        // XOR logical does not exist in Groovy :(
+        if ("$s1:$s2" ==~ /(:.+)|(.+:)/) throw new ArithmeticException()
 
-        if (s1.isEmpty() && s2.isEmpty()) 0
-        else if (s1 != s2) [s1.toCharArray(), s2.toCharArray()].transpose().findAll {n -> n[0] != n[1] }.size()
-         else 0
+        if (s1 == "") {
+            0
+        } else {
+            ((s1[0] == s2[0]) ? 0 : 1) + distance(s1.substring(1), s2.substring(1))
+        }
     }
 }
