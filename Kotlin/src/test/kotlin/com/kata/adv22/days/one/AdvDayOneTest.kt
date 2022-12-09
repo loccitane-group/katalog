d1f80234aee81939077f221f1bd47c69d623b95b
Jordan-Afonso-OCC <jordan.afonso.ext@loccitane.com>
2022-12-09 14:48:30 +0100

    refacto day one with right file access

--- Kotlin/src/test/kotlin/com/kata/adv22/days/one/AdvDayOneTest.kt
+++ Kotlin/src/test/kotlin/com/kata/adv22/days/one/AdvDayOneTest.kt
@@ -1,37 +1,41 @@
 package com.kata.adv22.days.one
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
 
 class AdvDayOneTest {
     @Test
     fun should_return_50000_for_example_file() {
-        val text = File("/Users/jordan/Documents/missions/loccitane/katas/katalog/Kotlin/src/test/kotlin/com/kata/adv22/inputTest.txt").readText()
+        val path = "${System.getProperty("user.dir")}/src/test/kotlin/com/kata/adv22/days/one/inputTest.txt"
+        val text = File(path).readText()
         val elfes = AdvDayOne(text)
-        assertEquals(50000,elfes.getMaxCalory())
+        assertEquals(50000, elfes.getMaxCalory())
     }
 
     @Test
     fun should_give_max_calory_of_day_one_input() {
-        val text = File("/Users/jordan/Documents/missions/loccitane/katas/katalog/Kotlin/src/test/kotlin/com/kata/adv22/inputDayOne.txt").readText()
+        val path = "${System.getProperty("user.dir")}/src/test/kotlin/com/kata/adv22/days/one/inputDayOne.txt"
+        val text = File(path).readText()
         val elfes = AdvDayOne(text)
-        assertEquals(67633,elfes.getMaxCalory())
+        assertEquals(67633, elfes.getMaxCalory())
     }
 
 
     @Test
     fun should_return_top3_max_calory_for_example_input() {
-        val text = File("/Users/jordan/Documents/missions/loccitane/katas/katalog/Kotlin/src/test/kotlin/com/kata/adv22/inputTest.txt").readText()
+        val path = "${System.getProperty("user.dir")}/src/test/kotlin/com/kata/adv22/days/one/inputTest.txt"
+        val text = File(path).readText()
         val elfes = AdvDayOne(text)
-        assertEquals(85000,elfes.getSumOfTop3MaxCalory())
+        assertEquals(85000, elfes.getSumOfTop3MaxCalory())
     }
 
 
     @Test
     fun should_return_top3_max_calory_of_day_one_input() {
-        val text = File("/Users/jordan/Documents/missions/loccitane/katas/katalog/Kotlin/src/test/kotlin/com/kata/adv22/inputDayOne.txt").readText()
+        val path = "${System.getProperty("user.dir")}/src/test/kotlin/com/kata/adv22/days/one/inputDayOne.txt"
+        val text = File(path).readText()
         val elfes = AdvDayOne(text)
-        assertEquals(199628,elfes.getSumOfTop3MaxCalory())
+        assertEquals(199628, elfes.getSumOfTop3MaxCalory())
     }
 }
