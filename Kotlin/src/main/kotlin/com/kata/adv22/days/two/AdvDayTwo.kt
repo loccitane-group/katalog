a4c2729350e0aa5ef79deb1dd45a6e8127cfea02
Jordan-Afonso-OCC <jordan.afonso.ext@loccitane.com>
2022-12-09 01:29:15 +0100

    day 2 : implement draws

--- Kotlin/src/main/kotlin/com/kata/adv22/days/two/AdvDayTwo.kt
+++ Kotlin/src/main/kotlin/com/kata/adv22/days/two/AdvDayTwo.kt
@@ -2,15 +2,18 @@ package com.kata.adv22.days.two
 
 class AdvDayTwo(val round: List<String>) {
     fun getRoundScore(): Int {
-        val firstPlayerMove = round.first()
-        val secondPlayerMove = round.last()
         var score = 1
-        if (moves[firstPlayerMove] == moves[secondPlayerMove]) {
-            score *= 2
+        val drawValuePoints = 3
+        if (isDraw()) {
+            score = shapes.getValue(round.last()) + drawValuePoints
         }
         return score
     }
 
+    private fun isDraw(): Boolean {
+        return shapes.getValue(round.first()) == shapes.getValue(round.last())
+    }
+
 }
 
-val moves = mapOf("A" to 1, "B" to 2, "C" to 3, "X" to 1, "Y" to 2, "Z" to 3)
+val shapes = mapOf("A" to 1, "B" to 2, "C" to 3, "X" to 1, "Y" to 2, "Z" to 3)
--- Kotlin/src/test/kotlin/com/kata/adv22/days/two/AdvDayTwoTest.kt
+++ Kotlin/src/test/kotlin/com/kata/adv22/days/two/AdvDayTwoTest.kt
@@ -1,12 +1,24 @@
 package com.kata.adv22.days.two
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
 class AdvDayTwoTest {
-    @Test
-    fun should_get_score_of_round() {
-        val round = listOf("A", "X")
-        Assertions.assertEquals(2, AdvDayTwo(round).getRoundScore())
+    @TestFactory
+    fun should_get_score_of_round() = listOf(
+        listOf("A", "X") to 4,
+        listOf("A", "Y") to 8,
+        listOf("A", "Z") to 3,
+        listOf("B", "X") to 1,
+        listOf("B", "Y") to 5,
+        listOf("B", "Z") to 9,
+        listOf("C", "X") to 7,
+        listOf("C", "Y") to 2,
+        listOf("C", "Z") to 6
+    ).map { (round, expectedScore) ->
+        DynamicTest.dynamicTest("should get score $expectedScore for round $round") {
+            assertEquals(expectedScore, AdvDayTwo(round).getRoundScore())
+        }
     }
 }
