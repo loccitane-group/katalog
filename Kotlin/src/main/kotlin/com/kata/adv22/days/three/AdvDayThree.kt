523df15d760531888b0417ec57dcfea685065c25
Jordan-Afonso-OCC <jordan.afonso.ext@loccitane.com>
2022-12-09 15:21:29 +0100

    day 3 : refacto with parametrized tests

--- Kotlin/src/main/kotlin/com/kata/adv22/days/three/AdvDayThree.kt
+++ Kotlin/src/main/kotlin/com/kata/adv22/days/three/AdvDayThree.kt
@@ -4,8 +4,8 @@ class AdvDayThree {
     private val priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     fun getSharedItemPriority(rucksackContent: String): Int {
-        val firstCompartment = rucksackContent.take(rucksackContent.length/2).toSet()
-        val secondCompartment = rucksackContent.takeLast(rucksackContent.length/2).toSet()
+        val firstCompartment = rucksackContent.take(rucksackContent.length / 2).toSet()
+        val secondCompartment = rucksackContent.takeLast(rucksackContent.length / 2).toSet()
         val sharedItem = firstCompartment.intersect(secondCompartment).elementAt(0)
 
         return priorities.indexOf(sharedItem) + 1
--- Kotlin/src/test/kotlin/com/kata/adv22/days/three/AdvDayThreeTest.kt
+++ Kotlin/src/test/kotlin/com/kata/adv22/days/three/AdvDayThreeTest.kt
@@ -1,12 +1,19 @@
 package com.kata.adv22.days.three
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
-class AdvDayThreeTest{
-    @Test
-    fun should_get_shared_item_priority() {
-        val rucksackContent = "abcdABCd"
-        assertEquals(4,AdvDayThree().getSharedItemPriority(rucksackContent))
+class AdvDayThreeTest {
+    @TestFactory
+    fun should_get_shared_item_priority() = listOf(
+        "abcdABCd" to 4,
+        "aaavaaabbbbvbb" to 22,
+        "AzertyAZERTY" to 27,
+        "qsdfghZjklmQSDFGHJKLZM" to 52,
+    ).map { (rucksackContent, expectedPriority) ->
+        DynamicTest.dynamicTest("should get priority $expectedPriority for rucksackContent $rucksackContent") {
+            assertEquals(expectedPriority, AdvDayThree().getSharedItemPriority(rucksackContent))
+        }
     }
 }
