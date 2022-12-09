529c7d771787e7d962af4ea5af564240117bc8ba
Jordan-Afonso-OCC <jordan.afonso.ext@loccitane.com>
2022-12-09 15:13:50 +0100

    day 3 : get priority of shared item

--- Kotlin/src/main/kotlin/com/kata/adv22/days/three/AdvDayThree.kt
+++ Kotlin/src/main/kotlin/com/kata/adv22/days/three/AdvDayThree.kt
@@ -1,7 +1,13 @@
 package com.kata.adv22.days.three
 
 class AdvDayThree {
+    private val priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     fun getSharedItemPriority(rucksackContent: String): Int {
-        return 4
+        val firstCompartment = rucksackContent.take(rucksackContent.length/2).toSet()
+        val secondCompartment = rucksackContent.takeLast(rucksackContent.length/2).toSet()
+        val sharedItem = firstCompartment.intersect(secondCompartment).elementAt(0)
+
+        return priorities.indexOf(sharedItem) + 1
     }
 }
