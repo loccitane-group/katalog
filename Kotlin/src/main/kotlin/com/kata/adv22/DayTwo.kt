f8763c0367a987d4fec6a4c68ce0ba4e42c3ef83
peven <peter.even@loccitane.com>
2022-12-20 23:08:55 +0100

    day02

---
+++ Kotlin/src/main/kotlin/com/kata/adv22/DayTwo.kt
@@ -0,0 +1,69 @@
+package com.kata.adv22
+
+const val Rock = 'A'
+const val Paper = 'B'
+const val Scissors = 'C'
+
+
+class DayTwo(private val rounds: List<Pair<Char, Char>>) {
+
+    private val paperScore = 2
+    private val rockScore = 1
+    private val scissorsScore = 3
+    private val winScore = 6
+    private val drawScore = 3
+    private val loseScore = 0
+    private val scoringMap: Map<Pair<Char, Char>, Int> =
+        mapOf(
+            Pair(Paper, Rock) to rockScore + loseScore,
+            Pair(Scissors, Rock) to rockScore + winScore,
+            Pair(Rock, Rock) to drawScore+ rockScore,
+            Pair(Rock, Paper) to paperScore + winScore,
+            Pair(Scissors, Paper) to paperScore + loseScore,
+            Pair(Paper, Paper) to drawScore + paperScore,
+            Pair(Rock, Scissors) to scissorsScore + loseScore,
+            Pair(Paper, Scissors) to scissorsScore + winScore,
+            Pair(Scissors, Scissors) to  drawScore + scissorsScore,
+        )
+
+    fun getScore(): Int {
+        return rounds.map {
+            when (it.second) {
+                'X' -> Pair(it.first, Rock) // lose
+                'Y' -> Pair(it.first, Paper)// draw
+                'Z' -> Pair(it.first, Scissors) // win
+                else -> throw Exception("unknown value ${it.second}")
+            }
+        }.fold(0) { acc, pair -> acc + scoringMap[pair]!! }
+    }
+
+    fun getScore2(): Int {
+        return rounds.map {
+            when (it.second) {
+                'X' -> Pair(it.first, getLoserFor(it.first)) // lose
+                'Y' -> Pair(it.first, it.first)// draw
+                'Z' -> Pair(it.first, getWinnerFor(it.first)) // win
+                else -> throw Exception("unknown value ${it.second}")
+            }
+        }.fold(0) { acc, pair -> acc + scoringMap[pair]!! }
+    }
+
+    private fun getLoserFor(choice: Char): Char {
+        return when (choice) {
+            Rock -> Scissors
+            Paper -> Rock
+            Scissors -> Paper
+            else -> throw Exception("Impossible value $choice")
+        }
+    }
+
+    private fun getWinnerFor(choice: Char): Char {
+        return when (choice) {
+            Rock -> Paper
+            Paper -> Scissors
+            Scissors -> Rock
+            else -> throw Exception("Impossible value $choice")
+        }
+    }
+
+}
---
+++ Kotlin/src/test/kotlin/com/kata/adv22/DayTwoTest.kt
@@ -0,0 +1,74 @@
+package com.kata.adv22
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class DayTwoTest : DayBase("peter") {
+
+    @Test
+    fun should_return_victory_for_rock_over_scissors() {
+        val round = listOf(Pair(Scissors,Rock))
+        val expected = 7
+        val shifumi = DayTwo(round)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_return_defeat_for_rock_over_paper() {
+        val round = listOf(Pair(Paper,Rock))
+        val expected = 0
+        val shifumi = DayTwo(round)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_return_draw_for_rock_over_rock() {
+        val round = listOf(Pair(Rock,Rock))
+        val expected = 3
+        val shifumi = DayTwo(round)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_return_victory_for_paper_over_rock() {
+        val round = listOf(Pair(Rock, Paper))
+        val expected = 8
+        val shifumi = DayTwo(round)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_return_victory_for_scissors_over_paper() {
+        val round = Pair(Paper, Scissors)
+        val expected = 9
+        val shifumi = DayTwo(listOf(round))
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_convert_XYZ_to_ABC() {
+        val input = "A X\n" + "B Y\n" + "C Z\n"
+        val lines:List<Pair<Char,Char>> = input.reader().readLines().map { Pair(it[0],it[2]) }
+        val expected = 9
+        val shifumi = DayTwo(lines)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_getScore_for_dayTwo_Part1() {
+        val lines:List<Pair<Char,Char>> = File("$path/$user/inputDayTwo.txt").reader().readLines().map { Pair(it[0],it[2]) }
+        val expected = getExpectedFor(user, 2, 1)
+        val shifumi = DayTwo(lines)
+        assertEquals(expected,shifumi.getScore())
+    }
+
+    @Test
+    fun should_getScore_for_dayTwo_Part2() {
+        val lines:List<Pair<Char,Char>> = File("$path/$user/inputDayTwo.txt").reader().readLines().map { Pair(it[0],it[2]) }
+        val expected = getExpectedFor(user, 2, 2)
+        val shifumi = DayTwo(lines)
+        assertEquals(expected,shifumi.getScore2())
+    }
+
+}
