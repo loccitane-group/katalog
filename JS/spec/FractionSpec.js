f587cfa2a67380836a3650a2bc851f8815e24d24
Eric TANG <eric.tang.ext@loccitane.com>
2024-10-23 11:10:19 +0200

    Fraction add refacto to addition fractionone and fractiontwo

--- JS/spec/FractionSpec.js
+++ JS/spec/FractionSpec.js
@@ -1,10 +1,13 @@
 const add = function (FractionOne, FractionTwo) {
-    return 0;
+    return FractionOne + FractionTwo;
 };
-
 
 describe('Fraction', () => {
     it('0 + 0 should return 0', () => {
         expect(add(0, 0)).toBe(0);
+    })
+
+    it('0 + 1 should return 1', () => {
+        expect(add(0, 1)).toBe(1);
     })
 });
