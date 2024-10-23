1419ba5ea64c3350bbf50f729fa209ed2b64be6c
Eric TANG <eric.tang.ext@loccitane.com>
2024-10-23 10:59:22 +0200

    Fraction add 0 + 0 = 1

---
+++ JS/spec/FractionSpec.js
@@ -0,0 +1,10 @@
+const add = function (FractionOne, FractionTwo) {
+    return 0;
+};
+
+
+describe('Fraction', () => {
+    it('0 + 0 should return 0', () => {
+        expect(add(0, 0)).toBe(0);
+    })
+});
