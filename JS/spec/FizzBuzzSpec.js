b6a2cebe9d88fd34498f7e6006d94155b0d1050e
ESSAGHIR Anas (External) <anas.essaghir.ext@loccitane.com>
2024-12-10 18:55:58 +0100

    show Fizz in fizzbuzz

--- JS/spec/FizzBuzzSpec.js
+++ JS/spec/FizzBuzzSpec.js
@@ -1,4 +1,4 @@
-const fizbuz = () => ["1","2"];
+const fizbuz = () => ["1", "2", "Fizz"];
 
 it("The first number should be 1", () => {
   var result = fizbuz();
@@ -6,6 +6,10 @@ it("The first number should be 1", () => {
 });
 
 it("The second number should display 2", () => {
-    var result = fizbuz();
-    expect(result[1]).toEqual("2");
+  var result = fizbuz();
+  expect(result[1]).toEqual("2");
+});
+it("The third number should display Fizz", () => {
+  var result = fizbuz();
+  expect(result[2]).toEqual("Fizz");
 });
