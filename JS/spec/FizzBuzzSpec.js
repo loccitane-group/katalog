daaeed42495d10860e5d1e7983fefa01a57aaf78
ESSAGHIR Anas (External) <anas.essaghir.ext@loccitane.com>
2024-12-10 19:46:09 +0100

    remove unuseful code

--- JS/spec/FizzBuzzSpec.js
+++ JS/spec/FizzBuzzSpec.js
@@ -1,20 +1,6 @@
-const fizzbuz = () => ["1", "2", "Fizz"];
+
 const isMultipleOfThree = (value) => value % 3 === 0;
 const isMultipleOfFive = (value) => value % 5 === 0;
-
-it("The first displayed value should be 1", () => {
-  var result = fizzbuz();
-  expect(result[0]).toEqual("1");
-});
-
-it("The second displayed value should be 2", () => {
-  var result = fizzbuz();
-  expect(result[1]).toEqual("2");
-});
-it("The third displayed value should be Fizz", () => {
-  var result = fizzbuz();
-  expect(result[2]).toEqual("Fizz");
-});
 
 it("1 is not multiple of 3", () => {
   var result = isMultipleOfThree(1);
